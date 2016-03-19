13072117315e929577e473f2df678d19fa5530cd
henr-y <mail@explodingcamera.com>
2016-03-19 12:12:35 +0100

    Added more cdn stuff

--- modifyHtml.js
+++ modifyHtml.js
@@ -2,8 +2,13 @@ const cheerio = require('cheerio');
 
 module.exports = function ($, config) {
   var cdn = '';
-  if (config.useCDN == true)
+  if (config.useCDN == true) {
     cdn = config.cdnUrl;
+    $('.scripts script').remove();
+    $('.scripts').append('<script src="' + cdn + '/lib/js/musiqpad-full.min.js"></script>');
+    console.log('Adding CDN links');
+  }
+
   $('script[src="/pads/lib/js/app.js"]').attr('src', cdn + '/lib/js/app.js');
   $('body').append('<script src="/lib/js/padplus.js"></script>');
   return $;
